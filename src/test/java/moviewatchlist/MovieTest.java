acb5e8340516cc21dbbf5a71cf0d4658d5a6f8a0
Jordan Lavigueure <lav.jordan@live.com>
2023-08-31 00:47:41 -0700

    created tests for equals method

--- src/test/java/moviewatchlist/MovieTest.java
+++ src/test/java/moviewatchlist/MovieTest.java
@@ -1,5 +1,49 @@
 package moviewatchlist;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
 public class MovieTest {
 	
+	@Test
+	public void equalsNull() {
+		Movie m = new Movie("TT123", "Superman", 2002);
+		assertFalse(m.equals(null));
+	}
+	@Test
+	public void equalsDifferentClass() {
+		Movie m = new Movie("TT123", "Superman", 2002);
+		assertFalse(m.equals(""));
+	}
+	@Test
+	public void equalsDifferentId() {
+		Movie a = new Movie("TT123", "Superman", 2002);
+		Movie b = new Movie("TT124", "Superman", 2002);
+		assertFalse(a.equals(b));
+	}
+	@Test
+	public void equalsDifferentTitle() {
+		Movie a = new Movie("TT123", "Superman", 2002);
+		Movie b = new Movie("TT123", "Batman", 2002);
+		assertFalse(a.equals(b));
+	}
+	@Test
+	public void equalsDifferentYear() {
+		Movie a = new Movie("TT123", "Superman", 2002);
+		Movie b = new Movie("TT123", "Superman", 2001);
+		assertFalse(a.equals(b));
+		assertFalse(b.equals(a));
+	}
+	@Test
+	public void equalsSelf() {
+		Movie a = new Movie("TT123", "Superman", 2002);
+		assertTrue(a.equals(a));
+	}
+	@Test
+	public void equalsIdenticalMovie() {
+		Movie a = new Movie("TT123", "Superman", 2002);
+		Movie b = new Movie("TT123", "Superman", 2002);
+		assertTrue(a.equals(b));
+	}
 }
