c69f45d852b54bd7b8ae0ed12bdd28b10ef3b901
Jordan Lavigueure <lav.jordan@live.com>
2023-08-31 02:22:24 -0700

    added ability to save and load MovieList in MovieList.json

--- src/main/java/moviewatchlist/MovieList.java
+++ src/main/java/moviewatchlist/MovieList.java
@@ -1,7 +1,15 @@
 package moviewatchlist;
 
+import java.io.FileWriter;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 public class MovieList {
 	private List<Movie> movies;
@@ -27,5 +35,45 @@ public class MovieList {
 		movies.add(movie);
 	}
 	
+	
+	//Read and write to file
+	
+	//Loads movie list from file
+	public boolean loadFromFile() {
+		try(InputStream in = MovieList.class.getResourceAsStream("/MovieList.json")){
+			String string = new String(in.readAllBytes(), StandardCharsets.UTF_8);
+			JsonArray list = (JsonArray) new Gson().fromJson(string, JsonObject.class).get("movies");
+			Gson gson = new Gson();
+			for(JsonElement element : list) {
+				add(gson.fromJson(element, Movie.class));
+			}
+			return true;
+		}catch(Exception e) {
+			return false;
+		}
+	}
+	
+	//Saves current movie list to file, overwrites all previous data.
+	public boolean saveToFile() {
+		//build output in String
+		Gson gson = new Gson();
+		StringBuilder json = new StringBuilder("{\"movies\": [");
+		if(!movies.isEmpty()) {
+			json.append(gson.toJson(movies.get(0)));
+			for(int i = 1; i < movies.size(); i++) {
+				json.append(",");
+				json.append(gson.toJson(movies.get(i)));
+			}
+		}
+		json.append("]}");
+		//Write string to file
+		try(FileWriter writer = new FileWriter("MovieList.json")){
+			writer.write(json.toString());
+			return true;
+		}catch(Exception e){
+			return false;
+		}
+		
+	}
 	
 }
