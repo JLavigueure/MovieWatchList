99f51c449bcea39c5c4318a6e5c4c0ee86808306
Jordan Lavigueure <lav.jordan@live.com>
2023-09-01 04:09:50 -0700

    Created Moving rating class

---
+++ src/main/java/moviewatchlist/MovieRating.java
@@ -0,0 +1,29 @@
+package moviewatchlist;
+
+public class MovieRating {
+	private double rating;
+	private int votes;
+	
+	public MovieRating(double rating, int votes) {
+		this.rating = rating;
+		this.votes = votes;
+	}
+	
+	public double getRating() {
+		return rating;
+	}
+	
+	public int getVotes() {
+		return votes;
+	}
+	
+	public void setRating(double rate) {
+		this.rating = rate;
+	}
+	
+	public void setVotes(int qty) {
+		votes = qty;
+	}
+	
+	
+}
